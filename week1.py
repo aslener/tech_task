f61fd53c8ab44fff5d3fcc5fe94baeccfbad42c3
Aslene <aslene6002@gmail.com>
2026-08-23 12:22:11 +0530

    task completed

--- week1.py
+++ week1.py
@@ -1,1 +1,17 @@
-print("hello world")
+import random
+target = random.randint(0,100)
+while True:
+    try:
+        guessed_num = int(input("guess the number:"))
+        if guessed_num<0 or guessed_num>100:
+            print("Enter a valid number between 0 and 100")
+        else:
+            if guessed_num==target:
+                print("You have guessed correctly")
+            elif guessed_num<target:
+                print("the number guessed is less than target number")
+            else:
+                print("the number guessed is greater than target number")
+    except Exception:
+        print("Please enter a valid number(a whole number between 0 and 100)")
+    
