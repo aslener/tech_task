bc40bb760bf5caec60756fb7d9137368e1551c96
Aslene <aslene6002@gmail.com>
2026-09-06 16:47:04 +0530

    added delete contact function

--- contactbk.py
+++ contactbk.py
@@ -1,5 +1,5 @@
 import json
-with open("contactbook.json") as f:
+with open("D:\\tech_tribe\\tech_task\\contactbook.json") as f:
     data = json.load(f)
 def add_new_contact():
     name=input("Enter name: ").capitalize()
@@ -14,7 +14,6 @@ def delete_contact():
     for contact in data["contacts"]:
         if contact["name"]==name:
             data["contacts"].remove(contact)
-
 with open("contactbook.json", "w") as f:
     json.dump(data, f)
 list_of_contacts()
